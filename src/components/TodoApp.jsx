9e025a77b449456ea33a7aa4ee28316d702383c9
noelfoka <nomgnenoel@gmail.com>
2023-12-06 06:38:36 +0100

    finish to build frontend part

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,34 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '@/routes/Home';
+import About from '@/routes/About';
+import Login from '@/routes/Login';
+import Profile from '@/routes/Profile';
+import NotMatch from '@/routes/NotMatch';
+import Layout from '@/components/Layout';
+import SinglePage from '@/routes/SinglePage';
+import ProtectedRoute from '@/components/ProtectedRoute';
+
 const TodoApp = () => {
-    return (
-      <div>
-        <h1>Hello world!</h1>
-        <p>I am in a React Component!</p>
-      </div>
-    );
-  };
-  export default TodoApp;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
+  );
+};
+export default TodoApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import TodoApp from "./components/TodoApp";
+import TodoApp from '@/components/TodoApp';
 
-const domContainer = document.getElementById("root");
+import '@/styles/app.css';
+// import './styles/app.css';
+
+const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
-root.render(<TodoApp />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TodoApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
